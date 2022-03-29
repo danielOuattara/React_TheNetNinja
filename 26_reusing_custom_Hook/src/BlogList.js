7fbeda453f76bb7b09c00bedeecbcdac357822a0
daniel ouattara <daniel.ouattara@gmx.com>
2022-03-29 07:33:40 +0200

    small correction

--- 26_reusing_custom_Hook/src/BlogList.js
+++ 26_reusing_custom_Hook/src/BlogList.js
@@ -5,19 +5,15 @@ const BlogList = (props) => {  // props is an object
 	
 	return (
 		<div className="blog-list">
-				<h2> { title } </h2>
-				{ blogs.map( blog => (
-					<div className="blog-preview" key={blog.id}>
-						<Link to={`/blogs/${blog.id}`}>
-							<h2>{blog.title}</h2>
-							<p>{blog.author}</p>
-							
-				
-							{/* <button onClick={() => handleHome(blog.id)}> ReturnHome </button>  */}
-							{/* <button onClick={() => props.handleDelete(blog.id)}> Delete </button>  */}
-						</Link>
-						</div>
-				))}
+			<h2> { title } </h2>
+			{ blogs.map( blog => (
+				<div className="blog-preview" key={blog.id}>
+					<Link to={`/blogs/${blog.id}`}>
+						<h2>{blog.title}</h2>
+						<p>{blog.author}</p>
+					</Link>
+				</div>
+			))}
 		</div>
 	);
 };
