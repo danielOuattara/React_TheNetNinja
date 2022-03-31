061e15ee97c42bea8d018747239addc3ef6d1a20
daniel ouattara <daniel.ouattara@gmx.com>
2022-03-31 11:36:28 +0200

    small formating

--- 20_making_a_custom_hook/src/NavBar.js
+++ 20_making_a_custom_hook/src/NavBar.js
@@ -1,20 +1,22 @@
 const NavBar = () => {
-    return ( 
+  return (
     <nav className="navbar">
-        <h1>The Dojo BLog</h1>
-        <div className="links">
-            <a href="/">Home</a>
-            <a href="/create" 
-               style= {
-                   {
-                       color:"white", 
-                       backgroundColor:"#f1356d",
-                       borderRadius:"8px"
-                    }
-                }>New Blog !</a>
-        </div>
+      <h1>The Dojo BLog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a
+          href="/create"
+          style={{
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px",
+          }}
+        >
+          New Blog !
+        </a>
+      </div>
     </nav>
-     );
-}
- 
+  );
+};
+
 export default NavBar;
--- 20_making_a_custom_hook/src/useFetch.js
+++ 20_making_a_custom_hook/src/useFetch.js
@@ -15,20 +15,17 @@
 //         setTimeout(() => {
 //             fetch(url)
 //                 .then(res => {
-//                     console.log("response =", res)
 //                     if (!res.ok) {
 //                         throw Error("Could not fetch the data from that ressource")
 //                     }
 //                     return res.json();
 //                 })
 //                 .then(data => {
-//                     console.log(data);
 //                     setData(data);
 //                     setIsPending(false);
 //                     setError(false);
 //                 })
 //                 .catch(err => {
-//                     // console.log("Error = ", err.message);
 //                     setError(err.message);
 //                     setIsPending(false);
 //                 });
