c747aeeee1a5bd852cb87324c62a38e3722da3e5
daniel ouattara <daniel.ouattara@gmx.com>
2022-06-02 23:23:59 +0200

    tiny changes

--- 20_making_a_custom_hook/src/useFetch.js
+++ 20_making_a_custom_hook/src/useFetch.js
@@ -37,34 +37,34 @@
 
 // export default useFetch;
 
-
 //--------------------------------------------------  ASYNC/AWAIT
 
 import { useState, useEffect } from "react";
 
 const useFetch = (url) => {
-    const [data, setData] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect( () => { // this function runs every render
-        (async () => {
-            try {
-                const res = await fetch(url);
-                if(!res.ok) throw Error("Could not fetch the data from that ressource")
-                const data = await res.json();
-                console.log(data)
-                setData(data);
-                setIsPending(false);
-                
-            } catch (err) {
-                setError(err.message);
-                setIsPending(false);
-            }
-        })()
-    }, [url]);
+  useEffect(() => {
+    // this function runs every render
+    (async () => {
+      try {
+        const res = await fetch(url);
+        if (!res.ok)
+          throw Error("Could not fetch the data from that ressource");
 
-    return {data, isPending, error};
-}
+        const data = await res.json();
+        setData(data);
+        setIsPending(false);
+      } catch (err) {
+        setError(err.message);
+        setIsPending(false);
+      }
+    })();
+  }, [url]);
+
+  return { data, isPending, error };
+};
 
 export default useFetch;
